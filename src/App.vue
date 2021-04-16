dd6d4ccbb892c82fbbaa40eb67dfef2309d36293
Dez <desmondadkins@gmail.com>
2021-04-16 16:02:36 -0400

    months and day function setup

--- src/App.vue
+++ src/App.vue
@@ -52,7 +52,39 @@ export default {
     setResults(results) {
       this.weather = results;
     },
-    todayDate() {},
+    todayDate() {
+      let d = new Date();
+      let months = [
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December",
+      ];
+      let days = [
+        "Sunday",
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday",
+      ];
+
+      let day = days[d.getDay()];
+      let date = d.getDate();
+      let month = months[d.getMonth()];
+      let year = d.getFullYear();
+
+      return `${day} ${date} ${month} ${year}`;
+    },
   },
 };
 </script>
