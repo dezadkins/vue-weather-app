d4fb147805f26b2bcea05fb6d39149bf64e97313
Dez <desmondadkins@gmail.com>
2021-04-16 14:10:20 -0400

    styled cold bg

--- src/App.vue
+++ src/App.vue
@@ -27,11 +27,24 @@ export default {
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  background-image: url("./assets/cold-bg.jpg");
+  background-size: cover;
+  background-position: bottom;
+  transition: 0.4%;
+}
+
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+body {
+  font-family: "montserrat", sans-serif;
+}
+
+main {
+  min-height: 100vh;
+  padding: 25%;
 }
 </style>
