d19ac1840f3a41346816fff10c4a1219057acf71
Dez <desmondadkins@gmail.com>
2021-04-16 14:16:04 -0400

    search box styling

--- src/App.vue
+++ src/App.vue
@@ -46,5 +46,28 @@ body {
 main {
   min-height: 100vh;
   padding: 25%;
+  background-image: linear-gradient(
+    to bottom,
+    rgba(0, 0, 0, 0.25),
+    rgba(0, 0, 0, 0.75)
+  );
+}
+
+.search-box {
+  width: 100%;
+  margin-bottom: 30px;
+}
+
+.search-box .search-bar {
+  display: block;
+  width: 100%;
+  padding: 15px;
+  color: #313131;
+  font-size: 20px;
+  appearance: none;
+  border: none;
+  outline: none;
+  background: none;
+  background-color: rgba(255, 255, 255, 0.5);
 }
 </style>
