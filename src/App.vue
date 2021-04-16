72c8c0adab60d102358593db296fc6f2383b8347
Dez <desmondadkins@gmail.com>
2021-04-16 15:57:35 -0400

    temp, place done

--- src/App.vue
+++ src/App.vue
@@ -11,27 +11,28 @@
         />
       </div>
 
-      <div class="weather-wrap">
+      <div class="weather-wrap" v-if="typeof weather.main != 'undefined'">
         <div class="location-box">
-          <div class="location">Columbus, OH</div>
-          <div class="date">Friday, April 16, 2020</div>
+          <div class="location">
+            {{ weather.name }}, {{ weather.sys.country }}
+          </div>
+          <div class="date">{{ weather.main.date }}</div>
         </div>
         <div class="weather-box">
-          <div class="temp">52°F</div>
-          <div class="weather-status">Rain</div>
+          <div class="temp">{{ Math.round(weather.main.temp) }}°F</div>
+          <div class="weather-status">{{ weather.weather[0].main }}</div>
         </div>
       </div>
     </main>
   </div>
 </template>
-
 <script>
 export default {
   name: "app",
   data() {
     return {
       api_key: "18833f51ee8cb3359a47f58f81a6c64b",
-      url_base: "https//api.openweathermap.org/data/2.5/",
+      url_base: "https://api.openweathermap.org/data/2.5/",
       query: "",
       weather: {},
     };
@@ -39,7 +40,9 @@ export default {
   methods: {
     fetchWeather(e) {
       if (e.key === "Enter") {
-        fetch(`${this.api_base}weather?q=${this.query}&APPID${this.api_key}`)
+        fetch(
+          `${this.url_base}weather?q=${this.query}&units=imperial&appid=${this.api_key}`
+        )
           .then((res) => {
             return res.json();
           })
@@ -49,6 +52,7 @@ export default {
     setResults(results) {
       this.weather = results;
     },
+    todayDate() {},
   },
 };
 </script>
